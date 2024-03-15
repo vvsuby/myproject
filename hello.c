c54cded25d1b19114abe1fac275274b7f2a1bdd1
lws <vvsubyi@gmail.com>
2024-03-15 10:53:48 +0900

    Revert "write sts hello.c"
    This reverts commit ae028ce54d8a23b79d4ffab4c788582c3f52c42a.

--- hello.c
+++ hello.c
@@ -3,8 +3,12 @@
 int main()
 {
     printf("Hello World\n");
+<<<<<<< HEAD
     printf("slay the spire\n");
     printf("cyberpunk\n");
     
+=======
+
+>>>>>>> parent of ae028ce (write sts hello.c)
     return 0;
 }
